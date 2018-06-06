a451bb7e1d6541e43cc8ba09e68049a909613fa1
tianyahechy <tianyahechu2004@163.com>
2018-06-06 21:04:40 +0800

    将三角形序列绘制到一个begin()end()中

--- core/src/Triangle.cpp
+++ core/src/Triangle.cpp
@@ -1,42 +1,48 @@
 #include "..\include\Triangle.h"
 
-Triangle::Triangle(std::vector<Pt3> pointVec, std::vector<float> colorVec)
+Triangle::Triangle(std::vector<std::vector<Pt3>> pointVec, std::vector<float> colorVec)
 {
-	_pointVec = pointVec;
+	_pointVecVec = pointVec;
 	_colorVec = colorVec;
 }
 
 Triangle::~Triangle()
 {
-	_pointVec.clear();
+	_pointVecVec.clear();
+	_colorVec.clear();
 }
 
 void Triangle::draw()
 {
 	glBegin(GL_TRIANGLES);
-	Pt3 p0 = _pointVec[0];
-	Pt3 p1 = _pointVec[1];
-	Pt3 p2 = _pointVec[2];
-
-	float p0_x = p0.x;
-	float p0_y = p0.y;
-	float p0_z = p0.z;
-	float p1_x = p1.x;
-	float p1_y = p1.y;
-	float p1_z = p1.z;
-	float p2_x = p2.x;
-	float p2_y = p2.y;
-	float p2_z = p2.z;
 
 	float r = _colorVec[0];
 	float g = _colorVec[1];
 	float b = _colorVec[2];
 	float a = _colorVec[3];
 
-	glColor4f(r,g,b,a);
+	glColor4f(r, g, b, a);
+	for (size_t i = 0; i < _pointVecVec.size(); i++)
+	{
+		std::vector<Pt3> theTriangle = _pointVecVec[i];
+		Pt3 p0 = theTriangle[0];
+		Pt3 p1 = theTriangle[1];
+		Pt3 p2 = theTriangle[2];
 
-	glVertex3f(p0_x, p0_y, p0_z);
-	glVertex3f(p1_x, p1_y, p1_z);
-	glVertex3f(p2_x, p2_y, p2_z);
+		float p0_x = p0.x;
+		float p0_y = p0.y;
+		float p0_z = p0.z;
+		float p1_x = p1.x;
+		float p1_y = p1.y;
+		float p1_z = p1.z;
+		float p2_x = p2.x;
+		float p2_y = p2.y;
+		float p2_z = p2.z;
+
+		glVertex3f(p0_x, p0_y, p0_z);
+		glVertex3f(p1_x, p1_y, p1_z);
+		glVertex3f(p2_x, p2_y, p2_z);
+	}
+	
 	glEnd();
 }
